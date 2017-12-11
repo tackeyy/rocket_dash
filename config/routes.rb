dc11fd17b65274db04931d113916d8c11570cdd7
tackeyy <takitayy@gmail.com>
2017-12-11 15:57:55 +0900

    Add Home

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'home/index'
+
+  root 'home#index'
+
+  resource :users do
+    resource :sign_up, controller: 'users/sign_up', only: %i[show create]
+    resource :sign_in, controller: 'users/sign_in', only: %i[show create]
+    resource :sign_out, controller: 'users/sign_out', only: %i[destroy]
+  end
 end
