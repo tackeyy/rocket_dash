f2dadf41b2deda9c6ad3dd1e4a07b511b7fe1799
tackeyy <takitayy@gmail.com>
2017-12-13 16:29:11 +0900

    Run rails g mailer UserMailer activation_needed_email activation_success_email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,9 +3,20 @@ class UserMailer < ApplicationMailer
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
-  #   en.user_mailer.reset_password_email.subject
+  #   en.user_mailer.activation_needed_email.subject
   #
-  def reset_password_email
+  def activation_needed_email
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.activation_success_email.subject
+  #
+  def activation_success_email
     @greeting = "Hi"
 
     mail to: "to@example.org"
