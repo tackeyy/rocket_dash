85407bc69b877b70ce2b50e00fde61d9f9ae78d8
tackeyy <takitayy@gmail.com>
2017-12-19 16:30:03 +0900

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  namespace :users do
-    get 're_send_activation_token/create'
-  end
-
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
 
   root 'home#index'
@@ -13,6 +9,8 @@ Rails.application.routes.draw do
     resource :sign_out, controller: 'users/sign_out', only: %i[destroy]
 
     resources :activate, controller: 'users/activate', only: %i[show]
+    resources :password_resets, controller: 'users/password_resets', except: %i[show destroy]
+
     resource :resend_activation_token, controller: 'users/resend_activation_token', only: %i[show create]
   end
 end
