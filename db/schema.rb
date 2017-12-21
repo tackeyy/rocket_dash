abf1de98bdb23d3d58fbcf3a6261131e8792c6e6
tackeyy <takitayy@gmail.com>
2017-12-21 19:01:35 +0900

    Change users table column order

--- db/schema.rb
+++ db/schema.rb
@@ -21,12 +21,12 @@ ActiveRecord::Schema.define(version: 20171213072158) do
     t.string "reset_password_token"
     t.datetime "reset_password_token_expires_at"
     t.datetime "reset_password_email_sent_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.datetime "deleted_at"
     t.string "activation_state"
     t.string "activation_token"
     t.datetime "activation_token_expires_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
     t.index ["activation_token"], name: "index_users_on_activation_token"
     t.index ["deleted_at"], name: "index_users_on_deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
